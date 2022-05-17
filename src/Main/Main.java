48dbfd448af2ceb87c751e8f9c96dee13d238521
Nagyvaradi Adam <adam.nagyvaradi@gmail.com>
2022-05-17 20:10:42 +0200

    Kezdetleges kockadobás + 20 oldalú előny és hátrány

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,15 +1,19 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         int count = 0;
-
+        Scanner scanner = new Scanner(System.in);
+        String roll = scanner.nextLine();
         while (count < 20) {
-            System.out.println(roll("1d8"));
+            // System.out.println(roll(roll)); // forma xd(4,6,8,10,12,20,100)
+            System.out.println(rolld20(roll));
             count++;
-        }
 
+        }
 
     }
 
@@ -57,11 +61,7 @@ public class Main {
                 return result;
             }
             case 20 -> {
-                while (rolled < count) {
-                    result = (int) ((Math.random() * 20) + 1);
-                    rolled++;
-                }
-                return result;
+                return (int) ((Math.random() * 20) + 1);
             }
             case 100 -> {
                 while (rolled < count) {
@@ -72,5 +72,34 @@ public class Main {
             }
         }
         return result;
+    }
+
+    public static int rolld20(){
+        return (int) ((Math.random() * 20) + 1);
+    }
+
+    public static int rolld20(String status){
+        int result1 = (int) ((Math.random() * 20) + 1);
+        int result2 = (int) ((Math.random() * 20) + 1);
+
+        if (status.equalsIgnoreCase("a")){
+                        if (result1 >= result2){
+                System.out.println("The rolles are " + result1 + " and "  + result2);
+                return result1;
+            } else {
+                            System.out.println("The rolles are " + result1 + " and "  + result2);
+                return result2;
+            }
+        } else if (status.equalsIgnoreCase("d")){
+            if (result1 <= result2){
+                System.out.println("The rolles are " + result1 + " and "  + result2);
+                return result1;
+            } else {
+                System.out.println("The rolles are " + result1 + " and "  + result2);
+                return result2;
+            }
+        } else {
+            return (int) ((Math.random() * 20) + 1);
+        }
     }
 }
